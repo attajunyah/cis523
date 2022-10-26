2f88fa43388db77c3d070bdbde734de65c62ad29
Frimpong Atta Junior Osei <61680263+attajunyah@users.noreply.github.com>
2022-10-26 10:48:01 -0700

    Update library.py

--- library.py
+++ library.py
@@ -4,6 +4,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 
 
